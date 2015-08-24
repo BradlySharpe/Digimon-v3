9e42b2537ad72f886f302d29a0ed262d9e2bb339
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-08-24 19:49:30 +1000

    Created Base Logic class

---
+++ server/Logic.php
@@ -0,0 +1,13 @@
+<?php
+  require ('DBase.php');
+
+  class Logic {
+    private $con;
+    private $db;
+
+    public function __construct($_con) {
+      $this->con = $_con;
+      $this->db = new DBase();
+    }
+
+  }
