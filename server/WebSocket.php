cb377ccc4110c6060a7fc052b74f95ecc9485125
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-08-24 20:05:53 +1000

    Cleaned up connection and logic

--- server/WebSocket.php
+++ server/WebSocket.php
@@ -1,5 +1,6 @@
 <?php
   require ('vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
+  require ('Logic.php');
   use Ratchet\Server\IoServer;
   use Ratchet\Http\HttpServer;
   use Ratchet\WebSocket\WsServer;
@@ -13,43 +14,37 @@
     public function __construct() {}
 
     public function onOpen(ConnectionInterface $con) {
-      // Store the new connection to send messages to later
-      //$this->clients->attach($con);
-      //echo "New connection! ({$con->resourceId})\n";
-      $this->clients[$con->resourceId] = array(
-        'con' => $con
-      );
+      $this->clients[$con->resourceId] = new Logic($con);
     }
 
-    public function onMessage(ConnectionInterface $from, $message) {
-      //echo sprintf('Received message from: %d - %s' . "\n", $from->resourceId, $message);
+    public function onMessage(ConnectionInterface $con, $message) {
+      if ($this->clients[$con->resourceId])
+        $this->clients[$con->resourceId]->receiveMessage($message);
+      else
+        $con->close();
     }
 
     public function onClose(ConnectionInterface $con) {
       // The connection is closed, remove it, as we can no longer send it messages
       //echo "Connection {$con->resourceId} has disconnected\n";
+      if ($this->clients[$con->resourceId])
+        $this->clients[$con->resourceId]->logout();
     }
 
     public function onError(ConnectionInterface $con, \Exception $ex) {
       //echo "An error has occurred: {$ex->getMessage()}\n";
+      // TODO: Log error
       $con->close();
     }
 
-    private function _getGame($con) {
-      return $this->clients[$con->resourceId]['game'];
-    }
   }
 
-  $webSocketServer = new WsServer(
-    new WebSocket()
-  );
+
+  $webSocketServer = new WsServer(new WebSocket());
   $webSocketServer->disableVersion(0);
   $webSocketServer->setEncodingChecks(!1);
 
-  $server = IoServer::factory(
-    new HttpServer($webSocketServer),
-    WebSocket::$port
-  );
+  $server = IoServer::factory(new HttpServer($webSocketServer), WebSocket::$port);
 
   echo "Server starting on port: " . WebSocket::$port . "\n";
   $server->run();
