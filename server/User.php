cf79b3e111b817bdd2e18f1b7bcd02451a95ff93
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-08-24 21:14:15 +1000

    Add generate token and handle message

--- server/User.php
+++ server/User.php
@@ -2,9 +2,19 @@
 
   class User {
     private $db;
+    private $token;
 
     public function __construct($_db) {
   		$this->db = $_db;
+      $this->token = time();
   	}
+
+    public function generateToken() {
+      return $this->token;
+    }
+
+    public function handleMessage($logic, $action, $data) {
+      
+    }
 
   }
