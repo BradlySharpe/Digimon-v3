e95cd405d9291ec5f6279c7e68e6b4892c8a34a4
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-08-25 20:29:06 +1000

    Regenerate database on start

--- start.php
+++ start.php
@@ -1,4 +1,47 @@
 <?php
+  echo "Populating Database!\n\n";
+  $con = mysql_connect('localhost', 'digimon', 'password') or
+    die('Error connecting to MySQL server: ' . mysql_error());
+
+  $lines = file('database.sql');
+  $templine = '';
+  $delimiter = ';';
+  foreach ($lines as $line)
+  {
+    if ('--' == substr($line, 0, 2) || '' == $line) {
+      echo "$line";
+      continue;
+    }
+
+    $matches = [];
+    preg_match('/^\s*delimiter\s+(.+)$/i', $line, $matches);
+    if ($matches && 2 == count($matches)) {
+      $delimiter = $matches[1];
+      continue;
+    }
+
+    $templine .= $line;
+    if ($delimiter == substr(trim($line), -strlen($delimiter), strlen($delimiter)))
+    {
+      $query = $templine;
+      $pos = strrpos($query, $delimiter);
+      if (false != $pos)
+        $query = substr_replace($query, '', $pos, strlen($delimiter));
+      if (!mysql_query($query)) {
+        echo "\n\nError re-populating database (delimiter: $delimiter):\n";
+        echo "Query:\n$query\n";
+        echo "Message:\n\t" . mysql_error();
+        die();
+      }
+      $templine = '';
+    }
+  }
+
+  mysql_close($con);
+  unset($con);
+
+  echo "\n\nDatabase Populated!\n\n";
+
   require ('server' . DIRECTORY_SEPARATOR . 'websocket.php');
 
   /*
